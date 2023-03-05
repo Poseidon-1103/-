38cf6eb3290bbe948f125386eb6789e6688f3275
拾叁 <1668267909@qq.com>
2023-03-05 14:06:06 +0800

    新建文件夹.jpg

---
+++ Violet & Grey/Assets/Scripts/Rounds/Sttlement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seetlement : MonoBehaviour
+{
+    public List<Card> seetlementList = new List<Card>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //点击按钮 开始结算回合
+    //获取当前回合所有敌人和玩家角色选择的牌 放到一个list里 
+
+
+}
