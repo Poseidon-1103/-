35a915dcd48d591414af0317483dbc3d8d9b794a
jilu <3089808861@qq.com>
2023-04-02 08:55:25 +0800

    更新

--- Violet & Grey/Assets/Scripts/Map/MapText.cs
+++ Violet & Grey/Assets/Scripts/Map/MapText.cs
@@ -24,6 +24,7 @@ public class MapText : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        /*grid = (Grid)GameObject.Find("grid");*/
         MapManage.GetInstance().InitMapInfo(map);
         // AStarNode[,] nodes = MapManage.GetInstance().nodes;
         // 获取角色的世界坐标
@@ -48,8 +49,8 @@ public class MapText : MonoBehaviour
             Vector3 endWorldPosition = Camera.main.ScreenToWorldPoint(mousePosition); // 将屏幕坐标转换为世界坐标
             endWorldPosition.z = 0;// 设置z轴值
             Vector3Int endCellPos = grid.WorldToCell(endWorldPosition);//将鼠标坐标转换成格子坐标，也就是终点坐标
-            Debug.Log("角色的世界坐标为："+playerPosition+",角色的格子坐标为："+playerCellPosition);
-            Debug.Log("鼠标点击的屏幕坐标为："+mousePosition+"鼠标点击的世界坐标为：" + endWorldPosition + "鼠标点击的格子坐标为：" + endCellPos);
+            Debug.Log("角色的世界坐标为：" + playerPosition + ",角色的格子坐标为：" + playerCellPosition);
+            Debug.Log("鼠标点击的屏幕坐标为：" + mousePosition + "鼠标点击的世界坐标为：" + endWorldPosition + "鼠标点击的格子坐标为：" + endCellPos);
             pathlist = MapManage.GetInstance().FindPath(playerCellPosition, endCellPos);//得到路径
             // if (pathlist!=null)
             // {
@@ -62,16 +63,16 @@ public class MapText : MonoBehaviour
         }
         //移动范围显示
         moveList = MapManage.GetInstance().MoveRange(playerCellPosition, actionValue, rangeMap);//得到可移动的范围地列表
-        if (moveList!=null)
+        if (moveList != null)
         {
             //清除之前的
             rangeMap.ClearAllTiles();
             foreach (var a in moveList)
             {
                 // Debug.Log(a);
-                rangeMap.SetTile(a,tileBase);//将可移动地位置高亮（设置显示的瓦片资源）
+                rangeMap.SetTile(a, tileBase);//将可移动地位置高亮（设置显示的瓦片资源）
             }
         }
     }
-    
+
 }
